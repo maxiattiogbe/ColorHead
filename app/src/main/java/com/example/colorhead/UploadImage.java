44821f9e165c0bbbb8720451cac48a8f6909a0a0
mna1024 <mnattiogbe@wpi.edu>
2021-03-16 21:46:16 -0400

    pH Calculation from RGB from Uploaded Image

--- app/src/main/java/com/example/colorhead/UploadImage.java
+++ app/src/main/java/com/example/colorhead/UploadImage.java
@@ -1,36 +1,51 @@
 package com.example.colorhead;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class UploadImage extends AppCompatActivity {
 
+    private Button uploadImageButton;
     private ImageView imageView;
     private TextView textView;
     private Bitmap bitmap;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
 
+        uploadImageButton = (Button) findViewById(R.id.uploadImageButton);
         imageView = (ImageView) findViewById(R.id.uploadImageImageView);
         textView = (TextView) findViewById(R.id.resultText);
 
         imageView.setDrawingCacheEnabled(true);
         imageView.buildDrawingCache(true);
 
+        //Make ImageView responsive to touch
         imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent){
@@ -352,6 +367,65 @@ public class UploadImage extends AppCompatActivity {
                 return true;
             }
         });
+
+        //Handle Upload Button click
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v){
+            // check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+              if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                  //permission not granted, request it.
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                //show popup for runtime permission
+                requestPermissions(permissions,PERMISSION_CODE);
+              } else{
+                // permission already granted
+                pickImageFromGallery();
+              }
+            } else {
+              //system os is less than lollipop
+              pickImageFromGallery();
+            }
+          }
+        });
     }
+
+  private void pickImageFromGallery() {
+      //intent to pick image
+      Intent intent = new Intent(Intent.ACTION_PICK);
+      intent.setType("image/*");
+      startActivityForResult(intent,IMAGE_PICK_CODE);
+  }
+
+  //handle the result of runtime permissions
+
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    switch(requestCode){
+      case PERMISSION_CODE:{
+        if(grantResults.length > 0 && grantResults[0] ==
+              PackageManager.PERMISSION_GRANTED){
+          //permission was granted
+          pickImageFromGallery();
+        } else {
+          //permission was denied
+          Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show();
+        }
+      }
+    }
+  }
+
+  // handle result of picked image
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE){
+      //set image to image view
+      imageView.setImageURI(data.getData());
+    }
+  }
+
 
 }
